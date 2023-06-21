15bf6568af9f951aef7924511071c1651187a9c5
Cesar Alejandro Ortiz Franco <a.ortiz@cignuz.com>
2023-06-20 23:02:59 -0600

    Se agrega la lista de roles

--- api/src/constants/index.ts
+++ api/src/constants/index.ts
@@ -1,1 +1,2 @@
 export { default as EnvVars } from './env-vars';
+export { default as Keys } from './keys';
---
+++ api/src/constants/keys.ts
@@ -0,0 +1,6 @@
+const roles = {
+  admin: 'ADMINISTRADOR',
+  otro: 'OTRO',
+};
+
+export default { roles };
