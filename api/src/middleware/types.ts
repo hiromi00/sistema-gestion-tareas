36ad065a3ab58dd55e38c3f7790ec6f13413d638
Cesar Alejandro Ortiz Franco <a.ortiz@cignuz.com>
2023-06-22 01:50:40 -0600

    Tipo usado para poder enviar los filtros de busqueda al request

--- api/src/middleware/types.ts
+++ api/src/middleware/types.ts
@@ -1,6 +1,11 @@
 import { Request } from 'express';
+import { TareaListReq } from '../domain/Tarea';
 
 export interface RequestWithUser extends Request {
+  user?: User;
+}
+
+export interface RequestWithUserAndFilters extends Request<{}, any, any, TareaListReq> {
   user?: User;
 }
 
